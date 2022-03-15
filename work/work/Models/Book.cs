b75ae8242802b7bca8164aae309d53c6e5fc00be
Nargiz Ganiyeva <tu7lyw21z@aztu.local>
2022-03-15 09:09:21 +0400

    first commit

---
+++ work/work/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace work.Models
+{
+    class Book:Product
+    {
+        public string Genre;
+
+        public Book(string Genre,int No, string Name, int Price): base(No,Name,Price)
+        {
+            this.Genre = Genre;
+        }
+        public string GetInfo()
+        {
+            return $"Kitab No: {No} - Adi: {Name} - Qiymeti: {Price} - Janri: {Genre}\nNo: {No} - Name: {Name} - Price: {Price}";
+        }
+    }
+}
---
+++ work/work/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using work.Models;
+namespace work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = GetInputInt("Nece eded kitab daxil edeceksizin?", 0);
+            Book[] info = new Book[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                int No = GetInputInt($"{i + 1}.ci kitabin nomresi: ", 1);
+                string Name = GetInputStr($"{i + 1}.ci kitabin adi: ",2,50 );
+                int Price = GetInputInt($"{i + 1}.ci kitabin qiymeti: ", 10);
+                int Count  = GetInputInt($"{i + 1}.ci kitabin deyeri: ", 15);
+                string Genre= GetInputStr($"{i + 1}.ci kitabin janri: ", 2, 5);
+
+                info[i] = new Book(Genre, No, Name, Price)
+                {
+                    Count = Count
+                };
+               
+                
+                
+                foreach (var a in info)
+                {
+                    Console.WriteLine(a.GetInfo());
+                }
+            }
+
+
+
+            static int GetInputInt(string name, int min, int max = int.MaxValue)
+            {
+                int input;
+                do
+                {
+                    Console.WriteLine(name );
+                    input = Convert.ToInt32(Console.ReadLine());
+                } while (input < min || input > max);
+
+                return input;
+            }
+            static string GetInputStr(string name, int min, int max = int.MaxValue)
+            {
+                string input;
+                do
+                {
+                    Console.WriteLine(name);
+                    input = Console.ReadLine();
+                } while (input.Length < min || input.Length > max);
+
+                return input;
+            }
+
+            
+            
+            
+        }
+    }
+}
